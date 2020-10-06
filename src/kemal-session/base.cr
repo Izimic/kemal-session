96301e73fe2716fc79b407f88cb23bb5341f310c
Izimic <mickael.bauer7@gmail.com>
2020-10-06 15:49:22 +0200

    re:samesite

--- src/kemal-session/base.cr
+++ src/kemal-session/base.cr
@@ -130,7 +130,7 @@ module Kemal
         secure: Session.config.secure,
         path: Session.config.path,
         domain: Session.config.domain,
-				samesite: 0,
+				samesite: HTTP::Cookie::SameSite::Strict,
       )
     end
 
